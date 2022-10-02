879e7ec88eb37c58e5993d5921319911cc719d2a
legostaev <alexey.legostaev@gmail.com>
2022-10-02 22:46:23 +0300

    solution of task 8 added

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите положительное число и я выведу все четные числа от 1 до Вашего числа:");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+Console.WriteLine($"Чётные числа от 1 до {number}");
+
+if (number == i || number == 0)
+{
+    Console.WriteLine($"в данном диапазоне нет четных чисел");
+}
+else if (number < 0)
+{
+    Console.WriteLine($"Ваше число должно быть больше 0");
+}
+else
+{
+    while (i < number)
+    {
+if (i % 2 != 1)
+{
+    Console.Write($"{i}, ");
+}
+var v = i++;
+    }
+}
